385cca92ec2d8f4adadf5a2cafc0182065111f63
Elias Afara <afaraelias@gmail.com>
2023-10-21 16:52:22 +0200

    chore: Refactor main.js for improved readability and modularity

--- main.js
+++ main.js
@@ -2,41 +2,56 @@ import { getFieldData } from "./api.js";
 import { buildGraph, findAllPaths } from "./graph.js";
 import getLogger from "./logger.js";
 
-// Define an asynchronous main function to execute the program
+// The ID of the starting asset
+const startAssetId = "-MA1551S-odms4rVbJ8K";
+
+// Function to log data to a file
+async function logDataToFile(logger, data, logName) {
+  logger.info(logName, { response: data });
+}
+
+// Function to fetch data and log it, handling errors
+async function fetchDataAndLog(projectId, subProjectId) {
+  try {
+    const fieldData = await getFieldData(projectId, subProjectId);
+    return fieldData;
+  } catch (error) {
+    console.error("An error occurred while fetching data:", error);
+    return null;
+  }
+}
+
 async function main() {
-  // Retrieve project and sub-project IDs from environment variables
   const projectId = process.env.PROJECT_ID;
   const subProjectId = process.env.SUB_PROJECT_ID;
 
-  const startAssetId = "-MA1551S-odms4rVbJ8K";
+  // Fetch and log the field data
+  const fieldData = await fetchDataAndLog(projectId, subProjectId);
 
-  try {
-    const fieldData = await getFieldData(projectId, subProjectId);
-
-    // Log the API response to a file for reference
+  // Check if data was successfully fetched
+  if (fieldData) {
+    // Logging the API response
     const apiResponseLogger = getLogger("response.log");
-    apiResponseLogger.info("API Response:", { response: fieldData });
+    await logDataToFile(apiResponseLogger, fieldData, "API Response");
 
     // Build a graph representation from the fetched field data
     const graphData = buildGraph(fieldData);
 
-    // Log the graph data for debugging and analysis
+    // Logging the graph data
     const graphLogger = getLogger("graph.log");
-    graphLogger.info("graph:", { response: graphData });
+    await logDataToFile(graphLogger, graphData, "Graph Data");
 
     // Find all possible paths in the graph starting from the specified asset
     const paths = findAllPaths(graphData, startAssetId);
 
-    // Log the discovered paths for reference and analysis
+    // Logging the discovered paths
     const pathsLogger = getLogger("paths.log");
-    pathsLogger.info("paths:", { response: paths });
+    await logDataToFile(pathsLogger, paths, "Paths");
 
     // Display the discovered paths as text to the console
     paths.forEach((path) => {
       console.log(path);
     });
-  } catch (error) {
-    console.error("An error occurred:", error);
   }
 }
 
